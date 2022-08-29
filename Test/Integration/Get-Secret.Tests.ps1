75430a1c56f76d31bb5f66c6e20d94ea1f47e391
Erik Whitesides <eklevjer@gmail.com>
2022-08-29 16:01:16 +0000

    updates to tests for Get-Secret

--- Test/Integration/Get-Secret.Tests.ps1
+++ Test/Integration/Get-Secret.Tests.ps1
@@ -3,41 +3,47 @@
 
 Describe 'Get-Secret' -Tag 'Integration' {
     BeforeAll {
-        #set vault environment variables
-        $VaultFile = "../.vault.json"
-        $VaultJson = Get-Content -Path $VaultFile | ConvertFrom-Json
-        $env:VAULT_ADDR  = $VaultJson.VAULT_ADDR
-        $env:VAULT_TOKEN = $VaultJson.VAULT_TOKEN
-
-        #pester vars
+        #module vars
         $ModulePath    = $env:PSModulePath.Split(':')[0]
         $ModuleName    = 'SecretManagement.HcVault.KV2'
         $SymLinkPath   = "$ModulePath/$ModuleName"
         $SymLinkTarget = (Get-Item -Path '../../Source').FullName
-        $VaultName     = 'pestertestvault'
-        $CacheDir      = "$env:HOME/$VaultName"
-        $CacheFilePath = "$env:HOME/$VaultName/.vault-token"
 
         #link our module to module path so
         #Microsoft.PowerShell.SecretManagement module can use it
         New-Item -ItemType 'SymbolicLink' -Path $SymLinkPath -Target $SymLinkTarget
+
+        #set vault env variables for vault cli
+        $VaultFile = '../.vault.json'
+        $VaultJson = Get-Content -Path $VaultFile | ConvertFrom-Json
+        $env:VAULT_ADDR  = $VaultJson.VAULT_ADDR
+        $env:VAULT_TOKEN = $VaultJson.VAULT_TOKEN
+
+        #vault vars
+        $VaultName       = 'pestertestvault'
+        $VaultMount      = 'secret'
+        $VaultPath       = 'creds'
+        $Script:VaultKey = 'mypass'
+        $Script:VaultVal = 'mysecret'
+        $CacheDir        = "$env:HOME/$VaultName"
+        $CacheFilePath   = "$env:HOME/$VaultName/.vault-token"
 
         #set token
         New-Item -ItemType 'Directory' -Path $CacheDir -Force | Out-Null
         Set-Content -Path $CacheFilePath -Value $env:VAULT_TOKEN
 
         #set a test secret with the vault cli
-        vault kv put -mount=secret creds password=supersecret
+        Invoke-Expression "vault kv put -mount=$VaultMount $VaultPath $VaultKey=$VaultVal"
 
         #register a test vault
         $RegisterParams = @{
             Name   = $VaultName
             Module = $ModuleName
             VaultParameters = @{
-                Server         = 'http://127.0.0.1:8200'
+                Server         = $env:VAULT_ADDR
                 ApiVersion     = '/v1'
-                Kv2Mount       = '/secret'
-                Kv2Path        = '/creds'
+                Kv2Mount       = "/$VaultMount"
+                Kv2Path        = "/$VaultPath"
                 AuthType       = 'Token'
                 TokenRenewable = $false #root token not renewable need to make test with another
                 TokenCachePath = $CacheFilePath
@@ -46,24 +52,29 @@ Describe 'Get-Secret' -Tag 'Integration' {
         }
         Register-SecretVault @RegisterParams
     }
-
-    It 'given specific key for Name, Get-Secret should return the value' {
-        Get-Secret -Vault $VaultName -Name 'password' -AsPlainText |
-        Should -Be 'supersecret'
-    }
-
-    It 'given * for Name, Get-Secret should return json string containing value' {
-        Get-Secret -Vault $VaultName -Name '*' -AsPlainText |
-        ConvertFrom-Json |
-        Select-Object -ExpandProperty 'password' |
-        Should -Be 'supersecret'
-    }
-
     AfterAll {
         #unregister vault
         Unregister-SecretVault -Name $VaultName
 
         #remove symlink
         Remove-Item -Path $SymLinkPath
+
+        #remove cache file and dir
+        Remove-Item -Path $CacheDir -Recurse -Force
+
+        #remove secrets at path creds
+        Invoke-Expression "vault kv metadata delete -mount=$VaultMount $VaultPath"
+    }
+
+    It 'given specific key for Name, Get-Secret should return the value' {
+        Get-Secret -Vault $VaultName -Name $VaultKey -AsPlainText |
+        Should -Be $VaultVal
+    }
+
+    It 'given * for Name, Get-Secret should return json string containing value' {
+        Get-Secret -Vault $VaultName -Name '*' -AsPlainText |
+        ConvertFrom-Json |
+        Select-Object -ExpandProperty $VaultKey |
+        Should -Be $VaultVal
     }
 }
