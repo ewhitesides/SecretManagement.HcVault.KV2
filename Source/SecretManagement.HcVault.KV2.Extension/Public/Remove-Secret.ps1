68f1bc47e3e521ffca3597ef6dc79ccce54b9ecf
Erik Whitesides <eklevjer@gmail.com>
2022-09-07 04:08:03 +0000

    initial

--- Source/SecretManagement.HcVault.KV2.Extension/Public/Remove-Secret.ps1
+++ Source/SecretManagement.HcVault.KV2.Extension/Public/Remove-Secret.ps1
@@ -1,19 +1,57 @@
 function Remove-Secret {
-    [CmdletBinding(SupportsShouldProcess)]
+<#
+.DESCRIPTION
+curl --header "X-Vault-Token: xxx" --request DELETE http://127.0.0.1:8200/v1/secret/data/creds
+#>
     param (
-        #Passed in from Remove-Secret -Name
         [Parameter(Mandatory)]
         [string]$Name,
 
-        #The VaultName of the secret, passed through from SecretManagement module
         [Parameter(Mandatory)]
         [Alias('Vault')]
         [string]$VaultName,
 
-        #Passed in from SecretManagement registered VaultParameters
         [Parameter(Mandatory)]
         [Alias('VaultParameters')]
-        [hashtable]$AdditionalParameters = (Get-SecretVault -Name $VaultName).VaultParameters
+        [hashtable]$AdditionalParameters
     )
-    Throw "not implemented"
+
+    #stop on all non-terminating errors in addition to terminating
+    $ErrorActionPreference = 'Stop'
+
+    #message
+    Write-Information "Getting secret $Name from vault $VaultName"
+
+    #set AdditionalParameters to shorter variable to stay within 80 column
+    $AP = $AdditionalParameters
+
+    #Validate AdditionalParameters
+    Test-VaultParameters $AP
+
+    #Construct uri
+    $Uri = $AP.Server + $AP.ApiVersion + $AP.Kv2Mount + '/data' + $Name
+
+    Try {
+        #try to delete secret using cached token
+        $Token = Get-CachedToken $AP
+        $Params = @{
+            Uri     = $Uri
+            Method  = 'Delete'
+            Headers = @{"X-Vault-Token" = "$Token"}
+        }
+        Invoke-RestMethod @Params | Out-Null
+    }
+    Catch {
+        #if it fails, try with a fresh token
+        $Token = Get-Token $AP
+        $Params = @{
+            Uri     = $Uri
+            Method  = 'Delete'
+            Headers = @{"X-Vault-Token" = "$Token"}
+        }
+        Invoke-RestMethod @Params | Out-Null
+
+        #set the token taht succeeded to the cache for next use
+        Set-CachedToken $AP.TokenCachePath $Token
+    }
 }
