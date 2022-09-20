23c2069b533f2ac52ee065447e4cedd125749ee9
Erik Whitesides <eklevjer@gmail.com>
2022-09-20 15:20:22 +0000

    fix so you can filter on Name

--- Source/SecretManagement.HcVault.KV2.Extension/Public/Get-SecretInfo.ps1
+++ Source/SecretManagement.HcVault.KV2.Extension/Public/Get-SecretInfo.ps1
@@ -18,7 +18,7 @@ function Get-SecretInfo {
     $ErrorActionPreference = 'Stop'
 
     #message
-    Write-Information "Getting secret info $Name from vault $VaultName"
+    Write-Information "Getting secret info $Filter from vault $VaultName"
 
     #set AdditionalParameters to shorter variable to stay within 80 column
     $AP = $AdditionalParameters
@@ -32,12 +32,16 @@ function Get-SecretInfo {
     Try {
         #try to get secret info using cached token
         $Token = Get-CachedToken $AP
-        New-SecretInfoObj -VaultName $VaultName -Uri $Uri -Token $Token -Depth 0
+
+        New-SecretInfoObj -VaultName $VaultName -Uri $Uri -Token $Token -Depth 0 |
+        Where-Object {$_.Name -like $Filter}
     }
     Catch {
         #if it fails, try with a fresh token
         $Token = Get-Token $AP
-        New-SecretInfoObj -VaultName $VaultName -Uri $Uri -Token $Token -Depth 0
+
+        New-SecretInfoObj -VaultName $VaultName -Uri $Uri -Token $Token -Depth 0 |
+        Where-Object {$_.Name -like $Filter}
 
         #set the token that succeeded to cache for next use
         Set-CachedToken $AP.TokenCachePath $Token
